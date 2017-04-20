7f7938b3045a1cd46e94a544c1150b2b70902479
bridge wang <tigerfan7495@gmail.com>
2017-04-20 11:30:17 -0400

    Update build_containers_1.0.0rc0.sh
    add quip_composite docker image

--- build/build_containers_1.0.0rc0.sh
+++ build/build_containers_1.0.0rc0.sh
@@ -4,6 +4,8 @@ git clone -b remove_mount_html https://github.com/camicroscope/ViewerDockerConta
 git clone https://github.com/camicroscope/OrderingService.git; 
 git clone https://github.com/camicroscope/DynamicServices.git;
 git clone https://github.com/SBU-BMI/findapi.git; 
+git clone -b quip_composite https://github.com/SBU-BMI/quip_cwl.git
+
 
 docker build -t quip_data DataDockerContainer
 docker build -t quip_loader LoaderDockerContainer
@@ -11,4 +13,4 @@ docker build -t quip_viewer ViewerDockerContainer
 docker build -t quip_jobs OrderingService 
 docker build -t quip_dynamic DynamicServices 
 docker build -t quip_findapi findapi
-
+docker build -t quip_composite quip_cwl
