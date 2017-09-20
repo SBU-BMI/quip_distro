dadef19b7da20572ffdaacc7f17d0f776f2f420a
Ryan Birmingham <birm@rbirm.us>
2017-09-20 11:48:48 -0400

    fix tests because no titles

--- tests/basic_test.js
+++ tests/basic_test.js
@@ -3,13 +3,12 @@ var url = "http://localhost/select.php";
 
 casper.test.begin('Link to flextables works fine', 3, function suite(test) {
     casper.start(url, function() {
-        test.assertTitle("caMicroscope", "caMicroscope homepage title is the one expected");
         test.assertExists('a[href="FlexTables/index.php"]', "link to camicroscope is found");
         this.click('a[href="FlexTables/index.php"]'); //click flex tables link
     });
 
     casper.then(function() {
-        test.assertTitle("Flex Tables", "Flextables works fine");
+        test.assertExists("#whoosh", "flex tables looks okay");
     });
 
     casper.run(function() {
