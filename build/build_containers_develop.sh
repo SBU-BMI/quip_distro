8d83068caccc1993964cc412c5de1c6f55ecf21b
Ganesh Iyer <lastlegion@gmail.com>
2017-11-15 14:54:42 -0500

    Update build_containers_develop.sh

--- build/build_containers_develop.sh
+++ build/build_containers_develop.sh
@@ -1,10 +1,10 @@
 
-git clone -b develop https://github.com/camicroscope/ViewerDockerContainer.git;
+#git clone -b develop https://github.com/camicroscope/ViewerDockerContainer.git;
 git clone -b develop https://github.com/camicroscope/DynamicServices.git;
 git clone -b develop https://github.com/SBU-BMI/quip_cwl.git
 
 
-docker build -t quip_viewer ViewerDockerContainer
+#docker build -t quip_viewer ViewerDockerContainer
 docker build -t quip_dynamic DynamicServices
 cd quip_cwl/node-kue; docker build -t quip_composite .
 cd ../..
