6e7498ead7ce557c0e407479cd0be1ca04c1d922
Ganesh Iyer <lastlegion@gmail.com>
2017-11-27 16:08:52 -0500

    Update select.php.js

--- tests/select.php.js
+++ tests/select.php.js
@@ -1,7 +1,7 @@
 var url = "http://localhost/select.php";
 
 
-casper.test.begin('Link to flextables works fine', 3, function suite(test) {
+casper.test.begin('Link to flextables works fine', 1, function suite(test) {
     casper.start(url, function() {
         test.assertExists('a[href="FlexTables/index.php"]', "link to camicroscope is found");
         this.click('a[href="FlexTables/index.php"]'); //click flex tables link
