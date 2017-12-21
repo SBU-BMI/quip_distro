b671032d1298faea2257d774a6846db0b185135d
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2017-12-21 12:42:30 -0500

    Fixed to work with current caMicroscope instance
    Missing '/'

--- tests/select.php.js
+++ tests/select.php.js
@@ -2,16 +2,16 @@ var url = "http://localhost/select.php";
 
 
 casper.test.begin('Link to flextables works fine', 1, function suite(test) {
-    casper.start(url, function() {
-        test.assertExists('a[href="FlexTables/index.php"]', "link to camicroscope is found");
-        this.click('a[href="FlexTables/index.php"]'); //click flex tables link
+    casper.start(url, function () {
+        test.assertExists('a[href="/FlexTables/index.php"]', "link to camicroscope is found");
+        this.click('a[href="/FlexTables/index.php"]'); //click flex tables link
     });
     /*
     casper.then(function() {
         test.assertExists("#whoosh", "flex tables looks okay");
     });
     */
-    casper.run(function() {
+    casper.run(function () {
         test.done();
     });
 });
