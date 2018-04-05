51254d9292df648a95c962424a38200b22c2a74e
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2018-04-05 10:52:32 -0400

    testing

--- tests/equality_test.sh
+++ tests/equality_test.sh
@@ -1,7 +1,27 @@
 #!/usr/bin/env bash
 
+exitStatus=0
+
 testEquality() {
-	assertEquals 1 1
+    branch_name='develop'
+    #UNCOMMENT!!!
+    #branch_name=$(git rev-parse --abbrev-ref HEAD)
+
+    script_name=$(find .. -name 'run_containers_*.sh')
+    substring=$(echo $script_name| cut -d'_' -f 3)
+    name=$(echo $substring| cut -d'.' -f 1)
+
+	assertEquals $branch_name $name
+	[[ $? == 1 ]] && exitStatus=1
+
+	script_name=$(find ../build -name 'build_containers_*.sh')
+	substring=$(echo $script_name| cut -d'_' -f 3)
+    name=$(echo $substring| cut -d'.' -f 1)
+
+	assertEquals $branch_name $name
+	[[ $? == 1 ]] && exitStatus=1
 }
 
 . shunit2-2.1.6/src/shunit2
+
+exit $exitStatus
