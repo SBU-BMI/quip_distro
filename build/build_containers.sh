1cabe4e0a11b6afefe51608eb5128a374d1bcf29
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2018-05-18 10:50:40 -0400

    cleanup

--- build/build_containers.sh
+++ build/build_containers.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
-git clone -b release https://github.com/camicroscope/DataDockerContainer.git;
+git clone "https://github.com/camicroscope/DataDockerContainer.git";
 cd DataDockerContainer; git checkout tags/2.0; cd ..
-git clone -b release https://github.com/camicroscope/LoaderDockerContainer.git;
+git clone "https://github.com/camicroscope/LoaderDockerContainer.git";
 cd LoaderDockerContainer; git checkout tags/2.0; cd ..
-git clone -b release https://github.com/camicroscope/ViewerDockerContainer.git;
+git clone "https://github.com/camicroscope/ViewerDockerContainer.git";
 cd ViewerDockerContainer; git checkout tags/2.0; cd ..
-git clone -b release https://github.com/camicroscope/OrderingService.git;
+git clone "https://github.com/camicroscope/OrderingService.git";
 cd OrderingService; git checkout tags/2.0; cd ..
-git clone -b release https://github.com/camicroscope/DynamicServices.git;
+git clone "https://github.com/camicroscope/DynamicServices.git";
 cd DynamicServices; git checkout tags/2.0; cd ..
-git clone -b release https://github.com/SBU-BMI/findapi.git;
+git clone "https://github.com/SBU-BMI/findapi.git";
 cd findapi; git checkout tags/2.0; cd ..
-git clone -b release https://github.com/SBU-BMI/quip_cwl.git
+git clone "https://github.com/SBU-BMI/quip_cwl.git";
 cd quip_cwl; git checkout tags/2.0; cd ..
 
 docker build -t quip_data:2.0 DataDockerContainer
