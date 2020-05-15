354ad3279d05e7e5b17d478a412bcdf8bab6ecf7
Ryan Birmingham <birm@rbirm.us>
2020-05-15 19:07:49 -0400

    for 3.7.4

--- Deps/Bindaas/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-java -jar -Dpid=BINDAAS_INSTANCE org.eclipse.osgi_3.10.100.v20150529-1857.jar
-
-while true; do sleep 1000; done
--- config/mongo_idx_pri.js
+++
@@ -1,1 +0,0 @@
-db.slide.createIndex({'study':1, 'specimen': 1, 'name': 1, 'collection': 1})
